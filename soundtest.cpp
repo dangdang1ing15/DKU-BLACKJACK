2e85ed562fe707700265dce899f123a9fd78c895
panoramaterrasse <72593461+panoramaterrasse@users.noreply.github.com>
2020-11-06 19:03:57 +0900

    Update soundtest.cpp
     주석 추가

--- soundtest.cpp
+++ soundtest.cpp
@@ -1,12 +1,14 @@
 ﻿#include <stdio.h>
+
 #include <Windows.h>
 #include <mmsystem.h>
 #pragma comment (lib, "winmm.lib")
 
-#define song "C:\\test\\song.wav"
+#define song "C:\\test\\song.wav" /*파일 위치로 지정*/
 void main()
 {
-	PlaySound(TEXT(song), NULL, SND_FILENAME | SND_ASYNC | SND_LOOP);
+	PlaySound(TEXT(song), NULL, SND_FILENAME | SND_ASYNC | SND_LOOP);  /*Playsound(주소, NULL, 옵션|옵션|옵션....)*/
+	                                                                   /*SND_ASYNC : 재생하면서 다음 코드 실행, SND_LOOP : 반복재생*/
 	while (1)
 	{
 		int a;
