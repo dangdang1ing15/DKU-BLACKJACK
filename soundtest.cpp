5bec5da8106458105210edc1ae06180db0fbd087
panoramaterrasse <72593461+panoramaterrasse@users.noreply.github.com>
2020-11-06 18:59:26 +0900

    Create soundtest.cpp
    test

---
+++ soundtest.cpp
@@ -0,0 +1,23 @@
+﻿#include <stdio.h>
+#include <Windows.h>
+#include <mmsystem.h>
+#pragma comment (lib, "winmm.lib")
+
+#define song "C:\\test\\song.wav"
+void main()
+{
+	PlaySound(TEXT(song), NULL, SND_FILENAME | SND_ASYNC | SND_LOOP);
+	while (1)
+	{
+		int a;
+		printf("종료?");
+		scanf_s("%d", &a);
+		if (a = 0)
+		{
+			PlaySound(NULL, 0, 0);
+			Sleep(1000);
+			printf("노래를 종료했습니다. 함수도 종료합니다.");
+			return;
+		}
+	}
+}
