ecc8d9e61fba23519537b34317a1dffeba916451
성혀니 <seonghyeoni@MacBook-Pro.local>
2020-11-10 15:30:28 +0900

    gotoxy 함수 수정, 메인함수 오류 수정

--- blackjack mainscreen added.c
+++ blackjack mainscreen added.c
@@ -61,7 +61,7 @@ void selectXY();
 void gameInfo();
 
 int main(){
-    init();
+    syssett();
     while (1) {
         drawbanner();
         int menuSelect() = selectXY();
@@ -319,11 +319,13 @@ void gotoxy(int x, int y){
 }
 
 int selectXY(){
-    gotoxy(24-2, 20);
+    int x = 24;
+    int y = 20;
+    gotoxy(x-2, y);
     printf("> 게임 시작");
-    gotoxy(24, 21);
+    gotoxy(x, y+1);
     printf("게임 정보");
-    gotoxy(24, 22);
+    gotoxy(x, y+2);
     printf("게임 종료");
     while (1) {
         int n = keycontrol();
