527caedd9559f012c60bcf4e331ff3639c653f9a
panoramaterrasse <72593461+panoramaterrasse@users.noreply.github.com>
2020-11-14 13:49:26 +0900

    노래
    노래 선정 및 저장 방법, 실제 사용할 코드

---
+++ music 2.cpp
@@ -0,0 +1,24 @@
+﻿#include <stdio.h>
+#include <Windows.h>
+#include <mmsystem.h>
+#pragma comment (lib, "winmm.lib")
+
+#define song "C:\\song.wav"
+
+void main()
+{
+	PlaySound(TEXT(song), NULL, SND_FILENAME | SND_ASYNC | SND_LOOP);
+	while (1)
+	{
+		int a;
+		printf("종료하려면 1을 입력하시오");
+		scanf_s("%d", &a);
+		if (a = 1)
+		{
+			PlaySound(NULL, 0, 0);
+			Sleep(1000);
+			printf("노래를 종료했습니다. 함수도 종료합니다.");
+			return;
+		}
+	}
+}
